4a88c8d28104866511fc7974692d931554055c2a
Gordon Choi <thefirate@gmail.com>
2023-08-05 14:11:02 +0900

    레이블까지 작성

---
+++ oldstar/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Gordon Choi on 2023/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
