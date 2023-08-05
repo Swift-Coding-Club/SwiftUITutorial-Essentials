86d5e0822ca8aaed3172817007b5a95f5a2974ed
DAEJINLIM <daejin.limm@gmail.com>
2023-08-05 14:42:17 +0900

    daejin

---
+++ daejin/daejin/daejinApp.swift
@@ -0,0 +1,17 @@
+//
+//  daejinApp.swift
+//  daejin
+//
+//  Created by 임대진 on 2023/08/05.
+//
+
+import SwiftUI
+
+@main
+struct daejinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
